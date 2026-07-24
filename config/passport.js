ddaa045591767048bfe6ee96b0d3fed94330f6b5
jessetusiime <jessetusiime078@gmail.com>
2026-07-24 15:44:55 +0300

    Fixed the callback URL

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ passport.use(
             clientID: process.env.CLIENT_ID,
             clientSecret: process.env.CLIENT_SECRET,
             callbackURL: process.env.CALLBACK_URL,
-            proxy: true // Add this for Render
+            proxy: true 
         },
         (accessToken, refreshToken, profile, done) => {
             return done(null, profile);
